394f998a1d3f10edd49f99909d67849a57c5e64a
rktrakesh <rtripathy324@gmail.com>
2026-04-01 03:56:35 +0530

    API documentation for Lockify (Swagger)

---
+++ src/main/java/authentication_backend/config/ApiDocsConfig.java
@@ -0,0 +1,31 @@
+package authentication_backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Lockify, Authentication Application",
+                version = "1.0",
+                description = "Application for authentication, that can use with any application.",
+                contact = @Contact(
+                        name = "Lockify Team",
+                        email = "rtripathy@gmail.com"
+                )
+        ),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class ApiDocsConfig {
+}
---
+++ src/main/java/authentication_backend/config/UrlConstant.java
@@ -0,0 +1,13 @@
+package authentication_backend.config;
+
+public class UrlConstant {
+
+    public static final String [] PUBLIC_URL = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/api/v1.0/auth/**"
+    };
+
+}
